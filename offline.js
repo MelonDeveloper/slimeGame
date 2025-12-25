a9dddb45bb02c00a4af9225dffc5fac0286072b5
Nick <nikita10020045@gmail.com>
2025-12-25 20:35:44 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1766683537,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/turret-sheet0.png",
+		"images/slime1-sheet0.png",
+		"images/slime1-sheet1.png",
+		"images/boolet-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
